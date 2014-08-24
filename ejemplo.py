c10c456bd2f11576749a835b0118d7a655fa38bf
Henrry96 <alumno.portillo@gmail.com>
2014-08-23 21:38:27 -0600

    valores
    operadores

---
+++ ejemplo.py
@@ -0,0 +1,36 @@
+y = True
+print(y)
+y = not y
+print(y)
+x = 4
+print(x)
+x = x + 4
+print(x)
+x = x + 2.25
+print(x)
+print(round(x))
+print(x / 1.15)
+print(x // 1.15)
+print(5 % 2)
+x = 4 / 2
+print(x)
+x = 4 // 3
+print(x)
+x = 9845 ** 24
+print(x)
+x = "Hola Variable"
+print(x)
+x = x + " tipo cadena"
+print(x)
+Pi = 3.141592654
+rad = 10
+A = Pi * (rad ** 2)
+print(A)
+C = 12
+F = (9 * C) / 5 + 32 
+print(F)
+masa = 72
+altura = 1.85
+IMC = masa / (altura ** 2)
+print(IMC)
+
